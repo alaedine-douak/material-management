5f6e82584a3235c7fea101a769f21838c6cef45d
Alaedindouak <alaedin.hak@gmail.com>
2024-04-20 15:08:24 +0100

    design and structure the MainWindow template

--- src/GM/App.xaml.cs
+++ src/GM/App.xaml.cs
@@ -1,13 +1,16 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 
 namespace GM;
 
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        MainWindow = new MainWindow();
+
+        MainWindow.Show();
+
+        base.OnStartup(e);
+    }
 }
 
