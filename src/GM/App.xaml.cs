1d4e38c47f06cf720dfbeba6b8404955350c4642
Alaedindouak <alaedin.hak@gmail.com>
2024-04-22 08:42:40 +0100

    configure hosting for application

--- src/GM/App.xaml.cs
+++ src/GM/App.xaml.cs
@@ -1,13 +1,35 @@
-﻿using System.Windows;
+﻿using GM.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
 
 namespace GM;
 
 public partial class App : Application
 {
+    private readonly IHost _host;
+
+    public App()
+    {
+        _host = Host.CreateDefaultBuilder()
+            .ConfigureServices(service =>
+            {
+
+                service.AddTransient<MainWindowViewModel>();
+
+                service.AddSingleton(s => new MainWindow
+                {
+                    DataContext = s.GetRequiredService<MainWindowViewModel>()
+                });
+            })
+            .Build();
+    }
+
     protected override void OnStartup(StartupEventArgs e)
     {
-        MainWindow = new MainWindow();
+        _host.Start();
 
+        MainWindow = _host.Services.GetRequiredService<MainWindow>();    
         MainWindow.Show();
 
         base.OnStartup(e);
